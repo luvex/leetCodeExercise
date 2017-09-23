bb0906026446ed8bf213b706a9469330667b8ba2
luvex <maxin_kevin@hotmail.com>
2017-09-23 23:46:33 +0200

    303 submitted

---
+++ src/RangeSumQueryImmutable/NumArray.java
@@ -0,0 +1,26 @@
+package RangeSumQueryImmutable;
+
+public class NumArray
+{
+	int[] sumArray;
+
+	public NumArray(int[] nums)
+	{
+		if (nums.length == 0)
+			return;
+		sumArray = new int[nums.length];
+		sumArray[0] = nums[0];
+		for (int i = 1; i < nums.length; i++)
+		{
+			sumArray[i] = sumArray[i - 1] + nums[i];
+		}
+	}
+
+	public int sumRange(int i, int j)
+	{
+		if (i == 0)
+			return sumArray[j];
+		else
+			return sumArray[j] - sumArray[i - 1];
+	}
+}
